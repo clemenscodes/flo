88f33e709e48443946fb734687d49d82a202a7ce
Flux Xu <fluxxu@gmail.com>
2024-11-13 14:44:48 -0500

    Add MDNS reg type for 2.0

--- crates/lan/src/mdns/mod.rs
+++ crates/lan/src/mdns/mod.rs
@@ -4,13 +4,18 @@ pub mod publisher;
 pub mod search;
 
 pub(crate) fn get_reg_type(game_version: &str) -> Result<String> {
-  let minor = game_version
-    .split(".")
-    .skip(1)
-    .take(1)
-    .next()
-    .ok_or_else(|| Error::InvalidVersionString(game_version.to_string()))?;
-  let num = format!("100{minor}")
+  let segments = game_version.split(".").collect::<Vec<&str>>();
+  if segments.len() != 4 {
+    return Err(Error::InvalidVersionString(game_version.to_string()));
+  }
+  let major = segments[0]
+    .parse::<i64>()
+    .map_err(|_| Error::InvalidVersionString(game_version.to_string()))?;
+  let major_offset = major - 1;
+  let minor = segments[1]
+    .parse::<i64>()
+    .map_err(|_| Error::InvalidVersionString(game_version.to_string()))?;
+  let num = format!("10{major_offset}{minor:02}")
     .parse::<i64>()
     .map_err(|_| Error::InvalidVersionString(game_version.to_string()))?;
 
@@ -26,5 +31,9 @@ fn test_get_reg_type() {
   assert_eq!(
     get_reg_type("1.34.0.00000").unwrap(),
     "_blizzard._udp,_w3xp2732"
+  );
+  assert_eq!(
+    get_reg_type("2.0.0.00000").unwrap(),
+    "_blizzard._udp,_w3xp2774"
   );
 }
